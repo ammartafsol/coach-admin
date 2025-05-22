9ee5c6f86b03870e492f81c81adb553726d269c0
wajahat-tafsol <wajahat.js@tafsol.com>
2025-05-22 13:44:02 +0500

    c

---
+++ src/component/organisms/UserProfile/UserProfile.jsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import classes from "./UserProfile.module.css";
+import BorderWrapper from "@/component/atoms/BorderWrapper";
+export default function UserProfile() {
+  return <BorderWrapper>UserProfile</BorderWrapper>;
+}
