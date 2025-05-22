57b8458ed23dac9623ec09d6265a877678b25398
ammartafsol <ammar.js@tafsol.com>
2025-05-22 22:01:42 +0500

    updated

--- src/component/organisms/FeedsCom/index.jsx
+++ src/component/organisms/FeedsCom/index.jsx
@@ -6,26 +6,61 @@ import { buttonsTabs } from '@/developmentContent/enums/enum';
 import { feedsData } from '@/developmentContent/dummyData';
 import FeedsCard from '@/component/molecules/FeedsCard/FeedsCard';
 import BorderWrapper from '@/component/atoms/BorderWrapper';
+import VideoModalSkeleton from "@/component/molecules/VideoModalSkeleton";
+import CommentsModal from "@/component/molecules/CommentsModal/CommentsModal";
 
 const FeedsCom = () => {
-    const [selectedTabs,setSelectedTabs] = useState(buttonsTabs[0]);
-  return (
-   <>
-   <div>
-        <ButtonTabs setSelectButton={setSelectedTabs} selectButton={selectedTabs} tabs={buttonsTabs} />
-    </div>
-    <div className={classes?.mainWrapper}>
-        <div className={classes.feedsList}>
-          {feedsData.map((feed, index) => (
-            <BorderWrapper key={index}>
-              <FeedsCard key={feed.id} feed={feed}  />
-            </BorderWrapper>
-          ))}
-        </div>
-      </div>
-   </>
-    
-  )
-}
+    const [selectedTabs, setSelectedTabs] = useState(buttonsTabs[0]);
+    const [isOpenVideo, setIsOpenVideo] = useState(false);
+    const [activeFeedId, setActiveFeedId] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
 
-export default FeedsCom
+    const handleOpenVideo = (feedId) => {
+        setActiveFeedId(feedId);
+        setIsOpenVideo(true);
+    };
+
+    const handleCloseVideo = () => {
+        setIsOpenVideo(false);
+        setActiveFeedId(null);
+    };
+
+    const activeFeed = activeFeedId !== null ? feedsData.find((feed) => feed.id === activeFeedId) : null;
+
+    return (
+        <>
+            <div>
+                <ButtonTabs setSelectButton={setSelectedTabs} selectButton={selectedTabs} tabs={buttonsTabs} />
+            </div>
+            <div className={classes?.mainWrapper}>
+                <div className={classes.feedsList}>
+                    {feedsData.map((feed, index) => (
+                        <BorderWrapper key={index}>
+                            <FeedsCard 
+                                key={feed.id} 
+                                feed={feed}
+                                setIsOpen={handleOpenVideo}
+                                onOpenComments={() => setIsOpen(true)}
+                            />
+                        </BorderWrapper>
+                    ))}
+                </div>
+            </div>
+
+            <CommentsModal
+                isOpen={isOpen}
+                onClose={() => setIsOpen(false)}
+                feedId={activeFeedId}
+            />
+
+            <VideoModalSkeleton
+                isOpen={isOpenVideo}
+                setIsOpenVideo={setIsOpenVideo}
+                onClose={handleCloseVideo}
+                activeFeed={activeFeed}
+            />
+        </>
+    );
+};
+
+export default FeedsCom;
