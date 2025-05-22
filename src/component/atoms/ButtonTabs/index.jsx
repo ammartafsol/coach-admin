3443c4f4d4bb1fd630855fd128032b34342bb4fd
ammartafsol <ammar.js@tafsol.com>
2025-05-22 15:02:02 +0500

    updated

---
+++ src/component/atoms/ButtonTabs/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "./ButtonTabs.module.css"
+
+const ButtonTabs = ([tabs,selectButton,setSelectButton]) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ButtonTabs
