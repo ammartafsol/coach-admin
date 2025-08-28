4ab8169e48c12a18cfcb25edc478812d960f0282
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-08-28 18:33:49 +0500

    clean

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,5 @@ export const config = {
   // apiBaseURL: "https://3wlwjj5k-3070.inc1.devtunnels.ms", // LOCAL
   // s3BucketURL: "https://tap2sos-backend-dd-8bd70d25feb3.herokuapp.com", // LIVE
   s3BucketURL: "https://storage.googleapis.com/the_coach_huddle",
-  googleMapApiKey: "AIzaSyAbi9IUF4TBu58oC9iGZexb045rMaQr2AQ",
   stripePublicKey: "",
 };
