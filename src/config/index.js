3efeba15c4ae0ce98c950188713e5fd0a610792f
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-09-11 16:49:29 +0500

    live

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,10 @@
 export const config = {
-  apiBaseURL: "https://k8d6sfpl-3068.inc1.devtunnels.ms",
+  // apiBaseURL: "https://r1g35c2r-3026.inc1.devtunnels.ms",
   // apiBaseURL: "https://wb6bv6gg-3070.inc1.devtunnels.ms",
-  // apiBaseURL: "https://coach-huddle-backend-dd2-d3911872f799.herokuapp.com", // live
+  apiBaseURL: "https://coach-huddle-backend-dd2-d3911872f799.herokuapp.com", // live
   // apiBaseURL: "https://3wlwjj5k-3070.inc1.devtunnels.ms", // LOCAL
   // s3BucketURL: "https://tap2sos-backend-dd-8bd70d25feb3.herokuapp.com", // LIVE
   s3BucketURL: "https://storage.googleapis.com/the_coach_huddle",
   stripePublicKey: "",
 };
+
