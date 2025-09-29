60520b0663c46056547e10305cf70397d926f4d0
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-09-29 20:41:45 +0500

    live link

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
-  apiBaseURL: "https://wb6bv6gg-3068.inc1.devtunnels.ms",
+  // apiBaseURL: "https://wb6bv6gg-3068.inc1.devtunnels.ms",
   // apiBaseURL: "https://wb6bv6gg-3070.inc1.devtunnels.ms",
-  // apiBaseURL: "https://coach-huddle-backend-dd2-d3911872f799.herokuapp.com", // live
+  apiBaseURL: "https://coach-huddle-backend-b11-4ff0eccc9a6f.herokuapp.com", // live
   // apiBaseURL: "https://3wlwjj5k-3070.inc1.devtunnels.ms", // LOCAL
   // s3BucketURL: "https://tap2sos-backend-dd-8bd70d25feb3.herokuapp.com", // LIVE
   s3BucketURL: "https://storage.googleapis.com/the_coach_huddle",
