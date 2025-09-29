714daa0d781fd662a02fc67e85aa3f31d53294c5
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-09-29 20:13:49 +0500

    pro

--- src/component/templates/AdminPages/CommissionsPage/CommissionsPage.jsx
+++ src/component/templates/AdminPages/CommissionsPage/CommissionsPage.jsx
@@ -1,18 +1,21 @@
 "use client";
-import TopHeader from '@/component/atoms/TopHeader';
-import React, { useEffect, useState } from 'react'
-import classes from './CommissionsPage.module.css'
-import AppTable from '@/component/organisms/AppTable/AppTable';
-import { tableHeadersCommissiosData } from '@/developmentContent/tableHeader';
-import ActionMenu from '@/component/molecules/ActionMenu/ActionMenu';
-import useDebounce from '@/resources/hooks/useDebounce';
-import { RECORDS_LIMIT } from '@/const';
-import useAxios from '@/interceptor/axiosInterceptor';
-import { EDIT_COMMISSION_OPTIONS, USER_ACTION_OPTIONS } from '@/developmentContent/dropdownOption';
-import FilterHeader from '@/component/molecules/FilterHeader/FilterHeader';
+import TopHeader from "@/component/atoms/TopHeader";
+import React, { useEffect, useState } from "react";
+import classes from "./CommissionsPage.module.css";
+import AppTable from "@/component/organisms/AppTable/AppTable";
+import { tableHeadersCommissiosData } from "@/developmentContent/tableHeader";
+import ActionMenu from "@/component/molecules/ActionMenu/ActionMenu";
+import useDebounce from "@/resources/hooks/useDebounce";
+import { RECORDS_LIMIT } from "@/const";
+import useAxios from "@/interceptor/axiosInterceptor";
+import {
+  EDIT_COMMISSION_OPTIONS,
+  USER_ACTION_OPTIONS,
+} from "@/developmentContent/dropdownOption";
+import FilterHeader from "@/component/molecules/FilterHeader/FilterHeader";
 
 export default function CommissionsPage() {
-    const { Get } = useAxios();
+  const { Get } = useAxios();
 
   const [showModal, setShowModal] = useState("");
   const [data, setData] = useState([]);
@@ -23,52 +26,46 @@ export default function CommissionsPage() {
   const [totalRecords, setTotalRecords] = useState(0);
   const [selectedItem, setSelectedItem] = useState(null);
 
-  const getData = async ({
-    pg = page,
-    _search = debounceSearch,
-  }) => {
-    
-        const params = {
-          page: pg,
-          search: _search,
-          limit: RECORDS_LIMIT,
-        };
-const query = new URLSearchParams(params).toString();
+  const getData = async ({ pg = page, _search = debounceSearch }) => {
+    const params = {
+      page: pg,
+      search: _search,
+      limit: RECORDS_LIMIT,
+    };
+    const query = new URLSearchParams(params).toString();
 
-    setLoading('loading')
+    setLoading("loading");
 
     const { response } = await Get({
       route: `admin/users/coach/all?${query}`,
     });
     if (response) {
       setData(response.data);
-       setPage(pg);
+      setPage(pg);
       setTotalRecords(response?.totalRecords);
     }
-    setLoading('')
-
+    setLoading("");
   };
 
-      useEffect(() => {
-        getData({
+  useEffect(() => {
+    getData({
       pg: 1,
       _search: debounceSearch,
     });
-      }, [debounceSearch]);
+  }, [debounceSearch]);
   return (
-     <>
+    <>
       <TopHeader title="Commissions">
         <FilterHeader
           inputPlaceholder="Search By Name"
           searchValue={search}
           customStyle={{ width: "300px" }}
-                     onChange={(e) => {
-             setSearch(e);
-             setPage(1);
-           }}
-         />
-
-       </TopHeader>
+          onChange={(e) => {
+            setSearch(e);
+            setPage(1);
+          }}
+        />
+      </TopHeader>
       <div>
         <AppTable
           tableHeader={tableHeadersCommissiosData}
@@ -85,14 +82,12 @@ const query = new URLSearchParams(params).toString();
             const rowItem = data[rowIndex];
             if (renderValue) return renderValue(item, rowItem);
             if (key === "action") {
-              return (
-                  <p className={classes.edit}>Edit</p>
-              );
+              return <p className={classes.edit}>Edit</p>;
             }
             return item || "";
           }}
         />
       </div>
     </>
-  )
+  );
 }
