efc483e709388f4bef7a287e0a50d44cd05886c5
developers032 <103639418+developers032@users.noreply.github.com>
2025-10-10 20:56:37 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['tap2sos-backend-dd-8bd70d25feb3.herokuapp.com',"storage.googleapis.com","wajba-bucket.s3.us-east-2.amazonaws.com",]
       },
+      output: "standalone",
 };
 
 export default nextConfig;
